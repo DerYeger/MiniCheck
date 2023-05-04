7d423f6e90d67e8b5ac7c45f42cb600b12d57dc5
Jan Müller <janmueller3698@gmail.com>
2023-05-04 16:06:06 +0200

    feat: add eof requirement

--- lib/Utils.hs
+++ lib/Utils.hs
@@ -1,9 +1,15 @@
-module Utils where
+module Utils (runParser) where
 
-import Text.Parsec (parse)
+import Text.Parsec (eof, parse)
 import Text.Parsec.String (Parser)
 
+withEof :: Parser a -> Parser a
+withEof p = do
+  result <- p
+  eof
+  return result
+
 runParser :: Parser a -> String -> a
-runParser p str = case parse p "" str of
+runParser p str = case parse (withEof p) "" str of
   Left err -> error $ "parse error at " ++ show err
   Right val -> val
