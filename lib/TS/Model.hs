34977d97337dd2fdcd0120aa020bd934e2da509b
Jan Müller <janmueller3698@gmail.com>
2023-05-04 16:30:22 +0200

    feat: implement transition parser

--- lib/TS/Model.hs
+++ lib/TS/Model.hs
@@ -2,10 +2,12 @@ module TS.Model where
 
 newtype State = State String deriving (Show, Eq)
 
-newtype Action = Action {actionName :: String} deriving (Show, Eq)
+newtype Action = Action String deriving (Show, Eq)
 
-newtype AtomicProposition = AtomicProposition {propName :: String} deriving (Show, Eq)
+newtype AtomicProposition = AtomicProposition String deriving (Show, Eq)
 
 type LabelingFunction = State -> [AtomicProposition]
 
-type TransitionSystem = ([State], [Action], [(State, Action, State)], [State], [AtomicProposition], LabelingFunction)
+type Transition = (State, Action, State)
+
+type TransitionSystem = ([State], [Action], [Transition], [State], [AtomicProposition], LabelingFunction)
--- lib/TS/Parser.hs
+++ lib/TS/Parser.hs
@@ -1,22 +1,35 @@
 module TS.Parser (parseTS) where
 
 import TS.Model
-import Text.Parsec (letter, many, newline, space, string)
+import Text.Parsec (char, letter, many, newline, space, string, (<|>))
 import Text.Parsec.String (Parser)
 import Utils (runParser)
 
-parseTS :: String -> State
-parseTS = runParser parseStates
+parseTS :: String -> TransitionSystem
+parseTS = runParser ts
 
-parseStates :: Parser State
-parseStates = keyword >> parseInitialState
-  where
-    keyword = do
-      _ <- string "States:"
-      newline
+ts :: Parser TransitionSystem
+ts = do
+  (initialState, states) <- parseStates
+  _ <- newline
+  transitions <- parseTransitions
+  let actions = map (\(_, action, _) -> action) transitions
+  let atomicPropositions = [] -- todo
+  let labelingFunction _ = [] -- todo
+  return (states, actions, transitions, [initialState], atomicPropositions, labelingFunction)
+
+parseStates :: Parser (State, [State])
+parseStates = do
+  _ <- string "States:" >> newline
+  initialState <- parseInitialState
+  otherStates <- many parseState
+  return (initialState, initialState : otherStates)
 
 arrow :: Parser String
 arrow = string "->"
+
+listItemStart :: Parser String
+listItemStart = string "-"
 
 parseInitialState :: Parser State
 parseInitialState = do
@@ -24,3 +37,30 @@ parseInitialState = do
   name <- many letter
   _ <- newline
   return (State name)
+
+parseState :: Parser State
+parseState = do
+  _ <- listItemStart >> space
+  name <- many letter
+  _ <- newline
+  return (State name)
+
+parseTransitions :: Parser [Transition]
+parseTransitions = do
+  _ <- string "Transitions:" >> newline
+  many parseTransition
+
+actionName :: Parser String
+actionName = do
+  many (letter <|> char '_')
+
+parseTransition :: Parser Transition
+parseTransition = do
+  _ <- listItemStart >> space
+  from <- many letter
+  _ <- space >> arrow >> space
+  action <- actionName
+  _ <- space >> arrow >> space
+  to <- many letter
+  _ <- newline
+  return (State from, Action action, State to)
