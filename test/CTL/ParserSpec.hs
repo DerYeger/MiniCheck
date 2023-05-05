a1e156466c9752e7b32b4206af1efeb771b44698
Jan Müller <janmueller3698@gmail.com>
2023-05-05 18:13:24 +0200

    test: verify derivation

--- test/CTL/ParserSpec.hs
+++ test/CTL/ParserSpec.hs
@@ -27,6 +27,14 @@ spec = do
       parseCTL "E (F a)" `shouldBe` Exists (transformEventually (Prop "a"))
     it "parses forall" $ do
       parseCTL "A (F a)" `shouldBe` ForAll (transformEventually (Prop "a"))
+    it "derives disjunction" $ do
+      parseCTL "(a || b)" `shouldBe` Negation (Conjunct (Negation (Prop "a")) (Negation (Prop "b")))
+    it "derives implication" $ do
+      parseCTL "(a -> b)" `shouldBe` Conjunct (Negation (Prop "a")) (Prop "b")
+    it "derives equivalence" $ do
+      parseCTL "(a <-> b)" `shouldBe` Conjunct (Conjunct (Negation (Prop "a")) (Prop "b")) (Conjunct (Negation (Prop "b")) (Prop "a"))
+    it "derives xor" $ do
+      parseCTL "(a xor b)" `shouldBe` Negation (Conjunct (Negation (Conjunct (Prop "a") (Negation (Prop "b")))) (Negation (Conjunct (Prop "b") (Negation (Prop "a")))))
   describe "path formula parser" $ do
     it "parses eventually" $ do
       parseCTL "E (F a)" `shouldBe` Exists (transformEventually (Prop "a"))
@@ -34,3 +42,11 @@ spec = do
       parseCTL "A (G a)" `shouldBe` Negation (ForAll (transformAlways (Prop "a")))
     it "parses until" $ do
       parseCTL "E (a U b)" `shouldBe` Exists (Until (Prop "a") (Prop "b"))
+    it "derives exists eventually" $ do
+      parseCTL "E (F a)" `shouldBe` Exists (Until (BoolLiteral True) (Prop "a"))
+    it "derives forall eventually" $ do
+      parseCTL "A (F a)" `shouldBe` ForAll (Until (BoolLiteral True) (Prop "a"))
+    it "derives exists always" $ do
+      parseCTL "E (G a)" `shouldBe` Negation (Exists (Until (BoolLiteral True) (Negation (Prop "a"))))
+    it "derives forall always" $ do
+      parseCTL "A (G a)" `shouldBe` Negation (ForAll (Until (BoolLiteral True) (Negation (Prop "a"))))
