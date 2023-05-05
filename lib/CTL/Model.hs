bac0456705efd35e80906f38b07dd53f8f18bc48
Jan Müller <janmueller3698@gmail.com>
2023-05-05 15:44:14 +0200

    feat: add ctl model

--- lib/CTL/Model.hs
+++ lib/CTL/Model.hs
@@ -1,1 +1,7 @@
 module CTL.Model where
+
+import TS.Model (AtomicProposition)
+
+data StateFormula = TrueLiteral | Prop AtomicProposition | Conjunct StateFormula StateFormula | Negation StateFormula | Exists PathFormula | ForAll PathFormula
+
+data PathFormula = Eventually StateFormula | Always StateFormula | Until StateFormula StateFormula
